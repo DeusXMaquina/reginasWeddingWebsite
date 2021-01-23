8d42d7dc223edac769df4eb695dcaa99c9b57d34
Pablo Altamirano <pabloaltamirano.a@gmail.com>
2021-01-22 21:32:45 -0600

    adding redux package and created example

--- Frontend/wedding-site/src/index.tsx
+++ Frontend/wedding-site/src/index.tsx
@@ -1,8 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
+import {createStore} from 'redux'
+
+const increment = () => {
+  return {
+    type: 'INCREMENT'
+  }
+}
+
+const decrement = () => {
+  return {
+    type: 'DECREMENT'
+  }
+}
+
+const counter:any = (state:number = 0, action:{type:string}) => {
+  switch(action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+  }
+}
+
+let store:any = createStore(counter)
+
+store.subscribe(() => console.log(store.getState()))
+
+store.dispatch(increment())
+store.dispatch(decrement())
 
 ReactDOM.render(
   <React.StrictMode>
