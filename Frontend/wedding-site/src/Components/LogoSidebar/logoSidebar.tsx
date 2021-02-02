139064300ec2404476b7b75017c4c8e11417985c
reginamt03 <regina.madero.torres@gmail.com>
2021-02-02 10:19:08 -0600

    modified logosidebar compontent, one box, aligned to logo and in upper right corner

--- Frontend/wedding-site/src/Components/LogoSidebar/logoSidebar.tsx
+++ Frontend/wedding-site/src/Components/LogoSidebar/logoSidebar.tsx
@@ -7,18 +7,18 @@ import { Box } from '@material-ui/core';
 const useStyles = makeStyles((theme: Theme) => ({
     root: {
       flexGrow: 1,
-      backgroundColor: theme.palette.background.paper,
+      backgroundColor: 'transparent',
       display: 'flex',
       height: '100px',
-      width: '100%',
+      width: '10%',
       color: theme.palette.primary.main,
+      marginLeft: '90%',
+      marginTop: '2%',
     },
     logo: {
       width: '63px',
       height: '82px',
       display: 'flex',
-      
-
     },
   }))
 
@@ -26,11 +26,9 @@ export default function LogoBar(){
     const classes = useStyles();
 
     return (
-<Box  className={classes.root} justifyContent='flex-end' position='fixed'>
-  <Box justifySelf='flex-end' m={3} p={2}>
+<Box  className={classes.root} justifySelf='flex-end' position='fixed' alignSelf='flex-end'>
     <img className={classes.logo} src={logo} alt="logo"/>
   </Box>
-</Box>
     )
 }
 
