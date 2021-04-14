6ee38fad2819c06797cd48d2fdae182cc7687d1d
reginamt03 <regina.madero.torres@gmail.com>
2021-04-14 12:40:37 -0500

    adding new @material-ui packages

---
+++ Frontend/wedding-site/src/Components/GiftPage/GiftBrands.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { makeStyles, Theme } from "@material-ui/core";
+import ContainerInvisible from "./../ContainerInvisible/ContainerInvisble";
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {},
+}));
+
+export default function GiftBrands() {
+  const classes = useStyles();
+
+  return (
+    ContainerInvisible
+  );
+}
