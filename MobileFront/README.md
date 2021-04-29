7f2c3a0cce108e42e110fdcabba8bf5d0df11881
Pablo Altamirano <pabloaltamirano.a@gmail.com>
2021-04-28 20:05:24 -0500

    main app structure

--- MobileFront/README.md
+++ MobileFront/README.md
@@ -4,3 +4,6 @@
 
 - [Drawer](https://material-ui.com/components/drawers/#swipeable)
 - [Box](https://material-ui.com/components/box/)
+- Countdown
+- RSVP
+- Google maps Address
--- MobileFront/wedding-mobile/src/App.tsx
+++ MobileFront/wedding-mobile/src/App.tsx
@@ -1,13 +1,22 @@
 import { Provider } from 'react-redux'
 import store from './Redux/Store/store'
+import logo from './pictures/SyR.png'
 import './App.css'
 
 export default function App() {
 
   return (
     <Provider store={store}>
-    <div>
-      Sup
+      <img className='logo' src={logo} alt='logo'/>
+    <div className='mainBox'>
+    <div  style={{textAlign: 'center', border: '2px solid red'}}>
+        Algo
+        <br/><button>Ola</button>
+      </div>
+      <div  style={{textAlign: 'center', border: '2px solid red'}}>
+        Algo
+        <br/><button>Ola</button>
+      </div>
     </div>
     </Provider>
   )
