f4c1fe2d02abee61bce19e43db50fd34248bf05f
Pablo Altamirano <pabloaltamirano.a@gmail.com>
2021-04-30 15:14:37 -0500

    basic drawer

--- MobileFront/wedding-mobile/src/App.tsx
+++ MobileFront/wedding-mobile/src/App.tsx
@@ -1,12 +1,14 @@
 import { Provider } from 'react-redux'
 import store from './Redux/Store/store'
 import logo from './pictures/SyR.png'
+import Drawer from './Components/Drawer/drawer'
 import './App.css'
 
 export default function App() {
 
   return (
     <Provider store={store}>
+      <Drawer/>
       <img className='logo' src={logo} alt='logo'/>
     <div className='mainBox'>
     <div  style={{textAlign: 'center', border: '2px solid red'}}>
