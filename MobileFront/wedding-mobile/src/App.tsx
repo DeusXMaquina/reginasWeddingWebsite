12c0172e7f65faad0b1cb6f2a632486f405105e2
reginamt03 <regina.madero.torres@gmail.com>
2021-05-13 14:46:37 -0500

    adding icons and GiftCard

--- MobileFront/wedding-mobile/src/App.tsx
+++ MobileFront/wedding-mobile/src/App.tsx
@@ -8,6 +8,7 @@ import Theme from './Theme/theme'
 import LocationCard from './Components/LocationCard/locationCard'
 import data from './CardsInfo/cardsInfo.json'
 import Home from './Components/Home/home'
+import GiftCard from './Components/Gift/gift'
 
 export default function App() {
   return (
@@ -16,10 +17,10 @@ export default function App() {
       <Drawer/>
       <img className='logo' src={logo} alt='logo'/>
       <div className= 'mainBox'>
-        <Home></Home>
+        <Home/>
         <LocationCard {...data.events.ceremony}/>
         <LocationCard {...data.events.party}/>
-  
+        <GiftCard/>
       </div>
     </Provider>
   </ThemeProvider>
