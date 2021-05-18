7c3cf889a5726fc91b41040a826ea9d792dab8ca
Pablo Altamirano <pabloaltamirano.a@gmail.com>
2021-05-18 18:24:14 -0500

    icons and images, logo, started rsvp

--- MobileFront/wedding-mobile/src/Components/DressCode/dresscode.tsx
+++ MobileFront/wedding-mobile/src/Components/DressCode/dresscode.tsx
@@ -6,38 +6,37 @@ import men from "../../pictures/codigo-hombre.png";
 
 
 const useStyles = makeStyles((theme: Theme)=>({
-    card: {
-        color: theme.palette.primary.main
-    }
+  card: {
+    color: theme.palette.primary.main
+  }
 }))
 
 export default function Dresscode () {
-    const classes = useStyles();
-    return (
-        <div className={classes.card}>
-            <ContentBox>
-            <Grid container spacing={4} direction='column' justify='center' alignContent='center'>
-                <Grid item xs={12}>
-                    <Typography>Dress Code</Typography>
-                </Grid>
-                <Grid item xs={12}>
-                    <img src={icon} alt="dresscode-icon" />
-                </Grid>
-                
-                <Grid container spacing={1} direction='row' justify='center'>
-                    <Grid item xs={5}>
-                        <img src={women} alt='womendresscode'></img>
-                    </Grid>
-                    <Grid item xs={2}>
-                    <Typography>Formal</Typography>
-                </Grid>
-                    <Grid item xs={5}>
-                        <img src={men} alt='mendresscode'></img>
-                    </Grid>
-                </Grid>
+  const classes = useStyles();
+  return (
+    <div className={classes.card}>
+      <ContentBox>
+        <Grid container spacing={4} direction='column' justify='center' alignContent='center'>
+          <Grid item xs={12}>
+            <Typography>Dress Code</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <img src={icon} alt="dresscode-icon" />
+          </Grid>
+
+          <Grid container spacing={1} direction='row' justify='center'>
+            <Grid item xs={5}>
+              <img src={women} alt='womendresscode'></img>
             </Grid>
-        </ContentBox>
-        </div>
-        
-    )
+            <Grid item xs={2}>
+              <Typography>Formal</Typography>
+            </Grid>
+            <Grid item xs={5}>
+              <img src={men} alt='mendresscode'></img>
+            </Grid>
+          </Grid>
+        </Grid>
+    </ContentBox>
+  </div>
+  )
 }
